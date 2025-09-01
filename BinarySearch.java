972c17f72fba2b7d3529e491af24e75ba373fd25
madhu967 <156585768+madhu967@users.noreply.github.com>
2025-09-01 06:36:55 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,20 +1,20 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n=nums.length;
-        int low=0,high=n-1;
-        
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(nums[mid]>target){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
+        int low = 0, high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;  //avoid overflow
+
+            if (nums[mid] == target) {
+                return mid; 
+            } else if (nums[mid] < target) {
+                low = mid + 1; 
+            } else {
+                high = mid - 1; 
             }
         }
-        return -1;
+
+        return -1; 
+
     }
 }
