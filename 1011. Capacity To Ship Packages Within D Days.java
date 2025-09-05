2f637916c5fcb0ecf49e81987dba91527a8086a5
madhu967 <156585768+madhu967@users.noreply.github.com>
2025-09-05 21:34:37 +0530

    Create 1011. Capacity To Ship Packages Within D Days.java

---
+++ 1011. Capacity To Ship Packages Within D Days.java
@@ -0,0 +1,35 @@
+class Solution {
+    public static int findDays(int[] weights,int cap){
+        int days=1;
+        int load=0;
+        int n=weights.length;
+        for(int i=0;i<n;i++){
+            if(load+weights[i]>cap){
+                days+=1;
+                load=weights[i];
+            }
+            else{
+                load+=weights[i];
+            }
+        }
+        return days;
+    }
+    public int shipWithinDays(int[] weights, int days) {
+        int low=Integer.MIN_VALUE,high=0;
+        for(int i=0;i<weights.length;i++){
+            high+=weights[i];
+            low=Math.max(low,weights[i]);
+        }
+        while(low<=high){
+            int mid=(low+high)/2;
+            int numOfDays=findDays(weights,mid);
+            if(numOfDays<=days){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+}
