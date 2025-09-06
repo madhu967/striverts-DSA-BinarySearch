16d6c9b04af7bd06ac384927c6e1e45f28f2ca84
madhu967 <156585768+madhu967@users.noreply.github.com>
2025-09-06 10:08:25 +0530

    Create 410. Split Array Largest Sum.java

---
+++ 410. Split Array Largest Sum.java
@@ -0,0 +1,39 @@
+class Solution {
+    public static int countPartitions(int[] nums, int maxSum) {
+        int n = nums.length; 
+        int partitions = 1;
+        long subarraySum = 0;
+        for (int i = 0; i < n; i++) {
+            if (subarraySum + nums[i] <= maxSum) {
+                
+                subarraySum += nums[i];
+            } else {
+               
+                partitions++;
+                subarraySum = nums[i];
+            }
+        }
+        return partitions;
+    }
+    public int splitArray(int[] nums, int k) {
+         int low = nums[0];
+        int high = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            low = Math.max(low, nums[i]);
+            high += nums[i];
+        }
+
+        //Apply binary search:
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int partitions = countPartitions(nums, mid);
+            if (partitions > k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+}
